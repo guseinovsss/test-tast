a3791264c40d9a33acc22aad16c507f7bc932066
unknown <guseinovsaid566@gmail.com>
2024-06-02 23:08:05 +0300

    done arabic calc

--- main.go
+++ main.go
@@ -5,13 +5,8 @@ import (
 	"strconv"
 )
 
-func arabic(a1, b1, c string) {
-	var a, err = strconv.Atoi(a1)
-	var b, err1 = strconv.Atoi(b1)
-	if nil == err {
-	}
-	if nil == err1 {
-	}
+func arabic(a, b int, c string) {
+
 	var r int = 0
 	switch c {
 	case "+":
@@ -32,10 +27,28 @@ func arabic(a1, b1, c string) {
 	}
 }
 
+func testForArabic(a1, b1, c string) int {
+	var a, err = strconv.Atoi(a1)
+	var b, err1 = strconv.Atoi(b1)
+	if nil != err || nil != err1 {
+		fmt.Println("Оба должны быть числом")
+		return 0
+	}
+
+	if (0 < a && a < 11) && (0 < b && b < 11) {
+		arabic(a, b, c)
+	} else {
+		fmt.Println("Числа должны быть больше от 1 до 10 включительно")
+		return 0
+	}
+
+	return 1
+}
+
 func main() {
 	var a, b, c string
 	fmt.Scan(&a, &c, &b)
 
-	arabic(a, b, c)
+	testForArabic(a, b, c)
 
 }
